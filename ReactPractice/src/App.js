a27447f2c3c7723875c3065ba670d2f8371b8a8d
seuha516 <seuha516@naver.com>
2021-03-15 21:25:14 +0900

    Revert "new"
    This reverts commit 0fe950066abec91fd670d881c17ffb4ac2a223bb.

--- ReactPractice/src/App.js
+++ ReactPractice/src/App.js
@@ -1,17 +1,8 @@
-import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import React from 'react';
+import ValidationSample from './ValidationSample';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <ValidationSample />;
+};
 
 export default App;
